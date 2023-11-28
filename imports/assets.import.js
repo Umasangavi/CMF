0019534179a1beba9168dee4dfb3ddbe7903c5f8
UMASANGAVI G <92853376+Umasangavi@users.noreply.github.com>
2023-11-28 17:12:08 +0530

    Add files via upload

---
+++ imports/assets.import.js
@@ -0,0 +1,117 @@
+//default assets imports
+
+//************** End of default assets ********************//
+//project assets imports
+
+import checked from "../assets/images/checked.png";
+import dropdown_arrow from "../assets/images/dropdown_arrow.png";
+import email_grey from "../assets/images/email_grey.png";
+import email_ping from "../assets/images/email_ping.png";
+import eye_open from "../assets/images/eye_open.png";
+import left_arrow from "../assets/images/left_arrow.png";
+import lock from "../assets/images/lock.png";
+import mobile_blue from "../assets/images/mobile_blue.png";
+import mobile_ping_1 from "../assets/images/mobile_ping_1.png";
+import mobile_ping from "../assets/images/mobile_ping.png";
+import mobile_yellow from "../assets/images/mobile_yellow.png";
+import padani from "../assets/images/padani.png";
+import tick_green from "../assets/images/tick_green.png";
+import tick_grey from "../assets/images/tick_grey.png";
+import music from "../assets/images/music.png";
+import band from "../assets/images/band.png";
+import school from "../assets/images/school.png";
+import organizer from "../assets/images/organizer.png";
+import group from "../assets/images/group.png";
+import notification from "../assets/images/notification.png";
+import filter from "../assets/images/filter.png";
+import toggle_btn from "../assets/images/toggle_btn.png";
+import close from "../assets/images/close.png";
+import menu from "../assets/images/option.png";
+import bookmark from "../assets/images/bookmark.png";
+import liked from "../assets/images/liked.png";
+import likes from "../assets/images/likes.png";
+import unlike from "../assets/images/unlike.png";
+import replay_active from "../assets/images/replay_active.png";
+import replay_inactive from "../assets/images/replay_inactive.png";
+import share from "../assets/images/share.png";
+import comment from "../assets/images/comment.png";
+import profile from "../assets/images/profile_empty.avif";
+import forward from "../assets/images/forward.png";
+import Delete from "../assets/images/delete.png";
+import unchecked from "../assets/images/unchecked.png";
+import checked_blue from "../assets/images/checked_blue.png";
+import radio_checked from "../assets/images/radio_checked.png";
+import radio_unchecked from "../assets/images/radio_unchecked.png";
+import note from "../assets/images/note.png";
+import map from "../assets/images/map.png";
+import edit from "../assets/images/edit.png";
+import user from "../assets/images/user.png";
+import plus_circle from "../assets/images/plus_circle.png";
+import sample_doc from "../assets/images/sample_doc.png";
+import add from "../assets/images/add.png";
+import msg from "../assets/images/msg.png";
+import greenPlus from "../assets/images/greenPlus.png";
+import yellowClose from "../assets/images/yellowClose.png";
+import attach from "../assets/images/attach.png";
+import camera from "../assets/images/camera.png";
+import mic from "../assets/images/mic.png";
+
+
+const Assets = {
+  camera,
+  mic,
+  checked,
+  attach,
+  yellowClose,
+  dropdown_arrow,
+  email_grey,
+  email_ping,
+  eye_open,
+  left_arrow,
+  lock,
+  mobile_blue,
+  mobile_ping_1,
+  mobile_ping,
+  mobile_yellow,
+  padani,
+  tick_green,
+  tick_grey,
+  music,
+  organizer,
+  school,
+  band,
+  group,
+  notification,
+  toggle_btn,
+  filter,
+  close,
+  menu,
+  bookmark,
+  liked,
+  likes,
+  unlike,
+  replay_active,
+  replay_inactive,
+  share,
+  comment,
+  profile,
+  forward,
+  Delete,
+  unchecked,
+  checked_blue,
+  radio_checked,
+  radio_unchecked,
+  map,
+  note,
+  edit,
+  user,
+  plus_circle,
+  sample_doc,
+  add,
+  msg,
+  greenPlus,
+
+  //default assets
+};
+
+export default Assets;
