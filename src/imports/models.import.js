e8f9ff7f57cfe897aaa52f212261a6a0ebf22ca3
UMASANGAVI G <92853376+Umasangavi@users.noreply.github.com>
2023-11-28 17:30:44 +0530

    Add files via upload

---
+++ src/imports/models.import.js
@@ -0,0 +1,27 @@
+import auth from '../model/auth.model';
+import general from '../model/general.model';
+import event from '../model/event.model';
+import comment from '../model/comment.model';
+import likes from '../model/likes.model'
+import search from '../model/search.model';
+import people from '../model/people.model';
+import chatBox from '../model/chatbox.model';
+
+
+
+
+
+
+export const Models = {
+  auth,
+  general,
+  event,
+  comment,
+  likes,
+  search,
+  people,
+  chatBox
+ 
+};
+
+export default Models;
